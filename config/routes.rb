bd71834bca65ab36c2349d17d982328c137e5452
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2015-12-22 18:07:25 -0800

    added everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
 
+
   root 'home#show'
   
-  get '/home' => 'home#show'
+  get '/resume' => 'resume#show', as: :resume
 
-  get '/about' => 'about#show'
+  get '/interests' => 'interests#show', as: :interests
+
+  get '/interests/music/garageband' => 'interests#garageband', as: :garageband
+
+  get '/interests/music' => 'interests#music', as: :music
+
+  get '/interests/volunteer' => 'interests#volunteer', as: :volunteer
+
+  get '/interests/travel' => 'interests#travel', as: :travel
+
+  get '/interests/travel/zimbabwe' => 'interests#zimbabwe', as: :zimbabwe
+
+  get '/interests/camping' => 'interests#camping', as: :camping
+
+  get '/interests/music/naked_voices' => 'interests#nakedvoices', as: :nakedvoices
+
+  get '/home' => 'home#show', as: :home
+
+  get '/about' => 'about#show', as: :about
+
+  get '/resume/download' => 'resume#download', as: :resume_download
 
 
   # The priority is based upon order of creation: first created -> highest priority.
